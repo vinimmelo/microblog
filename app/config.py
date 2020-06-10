ba13a4becee0ce399cd5fd06c10fbfc6ffea016a
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-06-09 21:02:04 -0300

    Adiciona configurações

--- app/config.py
+++ app/config.py
@@ -21,3 +21,4 @@ class Config:
     LANGUAGES = ["pt-BR", "en"]
     LOG_TO_STDOUT = os.getenv('LOG_TO_STDOUT')
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
